b6e980b06924a015955654f0f262a31e105934e5
Joeun <imjoeunha@gmail.com>
2017-02-11 01:00:28 +0900

    styling
    - 대략적인 스타일링

--- docs/src/js/sieum.js
+++ docs/src/js/sieum.js
@@ -7,13 +7,13 @@ $(function() {
     content: $('.poem.content')
   };
 
-  var sample_data = {
-    title: '호수',
-    author: '정지용',
-    content: '얼굴 하나야 손바닥 둘로 폭 가리지만 보고픈 마음 호수만 하니 눈 감을 밖에'
-  };
+  var sample_data = db[0];
 
   _.each($poem, function(elem, key) {
-    elem.text(sample_data[key]);
+    var string = sample_data[key];
+    if (string.match('  ')) {
+      string = string.replace(/(\s{2})+/g, '<br/>')
+    } 
+    elem.html(string);
   });
 })
