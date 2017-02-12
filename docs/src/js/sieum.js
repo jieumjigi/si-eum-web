9254d9b0a38f49374108a1c1f99e4f2533605458
Joeun <imjoeunha@gmail.com>
2017-02-12 22:46:18 +0900

    update: dynamic binding
    - 데이터를 동적으로 넣을 수 있도록 세팅

--- docs/src/js/sieum.js
+++ docs/src/js/sieum.js
@@ -1,19 +1,34 @@
-$(function() {
-  console.log("Welcome to SIEUM");
-
-  var $poem = {
-    title: $('.poem.title'),
-    author: $('.poem.author'),
-    content: $('.poem.content')
-  };
-
-  var sample_data = db[0];
-
-  _.each($poem, function(elem, key) {
-    var string = sample_data[key];
-    if (string.match('  ')) {
-      string = string.replace(/(\s{2})+/g, '<br/>')
+console.log("Welcome to SIEUM");
+var $mobile = $('.mobile');
+var container = `\
+    <div class="container">\
+      <div class="wrapper">\
+        <div class="poem title"></div>\
+        <div class="poem content"></div>\
+        <div class="deco"><div class="dash"></div></div>\
+        <div class="poem author"></div>\
+      </div>\
+    </div>`;
+       
+var data = _.map(db, function(obj, idx) {
+  $mobile.prepend(container);
+  _.each(obj, function(val, key) {
+    if (val.match(/\s\s/)) {
+      obj[key] = val.replace(/(\s{2})+/g, '<br/>')
     } 
-    elem.html(string);
   });
-})
+  return obj;
+});
+
+var today_data = data[0];
+$('.mobile .container').css('background-image', 'url(\'./src/img/' + today_data.img +'.jpg\')');
+
+var $poem = {
+  title: $('.poem.title'),
+  author: $('.poem.author'),
+  content: $('.poem.content')
+};
+
+_.each($poem, function(elem, key) {
+  elem.html(today_data[key]);
+});
